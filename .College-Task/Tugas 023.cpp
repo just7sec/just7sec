19d945dbfef1e2d28905edc1061c56efca2d1420
rafly <144429611+flywzen@users.noreply.github.com>
2025-02-24 19:46:42 +0700

    Update Tugas 023.cpp
    Signed-off-by: rafly  <144429611+flywzen@users.noreply.github.com>

--- .College-Task/Tugas 023.cpp
+++ .College-Task/Tugas 023.cpp
@@ -1,96 +1,55 @@
-//Latihan 2
+//Latihan 1
 //123140148
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
-//Deklarasi struct untuk menyimpan data mahasiswa
-struct Mahasiswa{
-    string nama,NIM;
-    int nilai;
-    //fungsi untuk overload data== (untuk membuat beberapa fungsi dengan nama yang sama tetapi dengan daftar parameter yang berbeda)
-    bool operator==(const string& search) const{
-        return NIM == search;
-    }
+//Deklarasi struct data Task
+struct Task{
+    string nama_tugas,deskripsi,deadline,status;
+    string *stat;
 };
-//Fungsi untuk menambah data
-void tambah(vector<Mahasiswa> *vec){
-    string nama,nim;
-    int nilai;
-    cout<<"Masukkan nama mahasiswa: ";
-    cin>>nama;
-    cout<<"Masukkan NIM mahasiswa: ";
-    cin>>nim;
-    cout<<"Masukkan nilai ujian mahasiswa: ";
-    cin>>nilai;
-    Mahasiswa data = {nama, nim, nilai};
-    vec->push_back(data);
+//Fungsi untuk mengubah status
+void Status(string *stat){
+    string Arrange;
+    cout<<"Masukkan status baru (To Do, In Progress, Done): ";
+    cin>>Arrange;
+    *stat = Arrange;
 }
-//Fungsi untuk mencari data dan mengembalikan posisi data
-vector<Mahasiswa>::iterator search(vector<Mahasiswa> *vec, string nim){
-    auto it = find(vec->begin(), vec->end(), nim);
-    if(it != vec->end()){
-        return it;
-    }else{
-        return vec->end();
-    }
+//Untuk menampilkan Message Selesai
+void Message(){
+    cout<<"Selesai";  
 }
-//Fungsi untuk menghapus data
-void hapus(vector<Mahasiswa> *vec){
-    string nim;
-    cout<<"Masukkan NIM yang ingin dihapus: ";
-    cin>>nim;
-    if(search(vec, nim) != vec->end()){
-        vec->erase(search(vec, nim));
-    }else{
-        cout<<"Tidak ada mahasiswa dengan NIM "<<nim<<endl;;
-    }
-}
-//Fungsi untuk mencari data
-void search(vector<Mahasiswa> *vec){
-    string nim;
-    cout<<"Masukkan NIM yang ingin dicari: ";
-    cin>>nim;
-    if(search(vec, nim) != vec->end()){
-        cout<<"Data ditemukan pada posisi "<<search(vec, nim) - vec->end() + 1<<endl;
-    }else{
-        cout<<"Tidak ada mahasiswa dengan NIM "<<nim<<endl;;
-    }
+//Print data dari struct Task
+void output(Task* Data){
+    
+    cout<<"Informasi Tugas: "<<Data->nama_tugas<<endl;
+    cout<<"Nama Tugas: "<<Data->deskripsi<<endl;
+    cout<<"Deskripsi: "<<Data->deadline<<endl;
+    cout<<"Deadline: "<<Data->deadline<<endl;
+    cout<<"Status: "<<Data->status<<endl;
 }
 
-int main(){
-    int x;
-    vector<Mahasiswa> mahasiswa;
-    vector<Mahasiswa> *opt = &mahasiswa;
-    bool s = 1;
-    //main menu
-    while(s){
-        cout<<"Menu:"<<endl;
-        cout<<"1. Tambah Data Mahasiswa"<<endl;
-        cout<<"2. Hapus Data Mahasiswa berdasarkan NIM"<<endl;
-        cout<<"3. Cari Data Mahasiswa berdasarkan NIM"<<endl;
-        cout<<"4. Keluar"<<endl;
-        cout<<"Pilihan: ";
-        cin>>x;
-        switch(x) {
-            case 1:
-                tambah(opt);
-                cout<<"Ukuran data saat ini: "<<mahasiswa.size()<<endl;
-                break;
-                
-            case 2:
-                hapus(opt);
-                cout<<"Ukuran data saat ini: "<<mahasiswa.size()<<endl;
-                break;
-            
-            case 3:
-                search(opt);
-                break;
-            
-            case 4:
-                s = 0;
-                cout<<"Program Selesai"<<endl;
-                break;
-        }
-    }
-}
+int main(){ //Program utama untuk mengkombinasikan dan memanggil semua fungsi
+    Task tugas;
+    tugas.stat = &tugas.status;
+    Task* Data = &tugas;
+    cout<<"Masukkan informasi tugas:"<<endl;
+    cout<<"Nama Tugas: ";
+    getline(cin, tugas.nama_tugas);
+    cout<<"Deskripsi: ";
+    getline(cin, tugas.deskripsi);
+    cout<<"Deadline: ";
+    getline(cin, tugas.deadline);
+    cout<<"Status: ";
+    getline(cin, tugas.status);
+    output(Data);
+    cout<<"Apakah Anda ingin mengubah status tugas? (y/n): ";
+    char answer;
+    cin>>answer;
+    //operator ternary
+    (answer == 'y' || answer == 'Y') ? Status(tugas.stat) : Message();
+    output(Data);
+
+    return 0;
+} 
+
